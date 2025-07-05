10a3592747df7f288fb675b0592a00baee9ec8fc
Taoshi <nicolaikh@live.dk>
2025-07-05 11:07:18 +0200

    setup initial server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/video", function (req, res) {
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
+});
+
+ // more code will go in here just befor the listening function
+
+app.listen(8000, function () {
+    console.log("Listening on port 8000!");
+});
